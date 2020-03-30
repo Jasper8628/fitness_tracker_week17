c6121989814bacaf7d6c26edcce52255f4be03ad
unknown <msavantfou@gmail.com>
2020-03-30 12:20:40 +1100

    commit

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
 const mongoose = require("mongoose");
-
+const express=require("express");
 const Workout = require("./models/workout");
 const seed = require("./seeders/seed");
+const db = require("./models");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkouts", { useNewUrlParser: true });
 
 
-Workout.create(seed[0])
-  .then(dbWorkout => {
-    console.log(dbWorkout);
+// db.Workout.create(seed)
+//   .then(dbWorkout => {
+//     console.log(dbWorkout);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//   });
+  
+app.get("/api/workouts/", (req,res)=>{
+  db.Workout.find({})
+    .then(dbWorkout =>{
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts/",({body},res)=>{
+  db.Workout.create(body)
+  .then(({_id})=>{
+    db.Workout.findByIdAndUpdate({},{$push:{workouts:_id}},{newa:true})
   })
-  .catch(({ message }) => {
-    console.log(message);
+  .then(dbWorkout =>{
+    res.json(dbWorkout);
+  })
+  .catch(err =>{
+    res.json(err);
   });
+});
+
+//app.put("/api/workouts/")
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
